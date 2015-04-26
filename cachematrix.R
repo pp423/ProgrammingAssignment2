9965cb858bac0fc7baa3625d9ed1687af1f244c6
Piyush <pp423@njit.edu>
2015-04-26 18:39:27 -0400

    assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The Program is to create a square matrix and give a inverse of it using 
+## function within function & <<- operator
 
-## Write a short comment describing this function
+## This function is only for creating a square matrix & the matrix stored in a 
+## variable here is used for reversing in the second function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the if the matrix is already reversed in the cache
+## if yes then it returns a message else gives a reverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
